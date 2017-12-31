b7b41656a96802e99642d8fcb5955183db1aa6b6
xqs002 <xqs002>
2017-12-31 21:17:08 +0800

    '创建用户模型'

--- sm/sm_login/models.py
+++ sm/sm_login/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Seller(models.Model):
+    s_name = models.CharField(max_length=32)
+    s_password = models.CharField(max_length=64)
+
+class Buyer(models.Model):
+    b_name = models.CharField(max_length=32)
+    b_password = models.CharField(max_length=64)
