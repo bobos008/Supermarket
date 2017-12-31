a8ec842b2fa7b0ba4dd5aa258822fd6edd606440
xqs002 <xqs002>
2017-12-31 23:33:09 +0800

    '修复加载模板错误'

--- sm/sm_login/views.py
+++ sm/sm_login/views.py
@@ -6,7 +6,7 @@ def test1(request, id1):
 
 # 用户注册
 def register(request):
-    return render(request, 'sm_login/register')
+    return render(request, 'sm_login/register.html')
 
 # 用户登陆
 def login(request):
