c27ed4d8efccc422328de0b0f9bbe4c1c0d6a2ab
xqs002 <xqs002>
2017-12-31 23:31:13 +0800

    '加载用户注册模板'

--- sm/sm_login/views.py
+++ sm/sm_login/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def register(request):
-    return HttpResponse('hello')
-
 def test1(request, id1):
 	return HttpResponse('test1=%s'%id1)
+
+# 用户注册
+def register(request):
+    return render(request, 'sm_login/register')
 
 # 用户登陆
 def login(request):
